8416744386a31cf106fc9f4f588638724e25b6bc
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-09-10 21:29:16 -0500

    some examples of lifecycle methods

--- src/main/java/com/springboot/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/springboot/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,23 @@
 package com.springboot.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
+    // define init method
+    @PostConstruct
+    public void initMethod() {
+        System.out.println("Init method: " + getClass().getSimpleName());
+    }
+
+    // define destroy method
+    @PreDestroy
+    public void destroyMethod() {
+        System.out.println("Destroy method: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
